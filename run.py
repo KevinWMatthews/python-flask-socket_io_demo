0993d2c904fd6a09f29b3b02a1fb64e708214952
Kevin Matthews <kmatthews@cyberdata.net>
2017-01-25 13:14:27 -0800

    Start a socketio app

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-from socket_io_demo import app
+from socket_io_demo import app, socketio
 import socket_io_demo.views
-app.run(host='127.0.0.1', port=5000)
+socketio.run(app, host='127.0.0.1', port=5000)
--- socket_io_demo/__init__.py
+++ socket_io_demo/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('SOCKET_IO_DEMO_SETTINGS', silent = True)
+socketio = SocketIO(app)
 
 from views.default import default_view
 
