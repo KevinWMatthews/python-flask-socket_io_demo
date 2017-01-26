0c1fa14ba5f03c6bff5c76a20f22d6106d29212e
Kevin Matthews <kmatthews@cyberdata.net>
2017-01-25 16:07:19 -0800

    Create and connect sockets.

--- socket_io_demo/views/default.py
+++ socket_io_demo/views/default.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, abort
 from jinja2 import TemplateNotFound
-from socket_io_demo import app
+from socket_io_demo import app, socketio
 
 default_view = Blueprint('index', __name__)
 
@@ -11,3 +11,11 @@ def show(page):
         return render_template('%s.html' % page)
     except TemplateNotFound:
         abort(404)
+
+@socketio.on('connect')
+def handle_connect():
+    print 'connected'
+
+@socketio.on('my event')
+def handle_json(json):
+    print 'received my event: ' + str(json)
