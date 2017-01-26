a9e706e85548f75bd70069ddfe1bfc249fa3d953
Kevin Matthews <kmatthews@cyberdata.net>
2017-01-25 17:42:33 -0800

    Emit and receive events in both python and javascript.

--- socket_io_demo/static/custom_socket_io.js
+++ socket_io_demo/static/custom_socket_io.js
@@ -1,4 +1,102 @@
+// Reference socket.io-client at:
+// https://github.com/socketio/socket.io-client
+
+var status_log = document.getElementById('status_log');
+
 var socket = io.connect('http://' + document.domain + ':' + location.port);
+
 socket.on('connect', function() {
-    socket.emit('my event', {data: 'I\'m connected!'});
+    message = 'SocketIO connected';
+    console.log(message);
+    logStatus(status_log, message);
 });
+
+socket.on('disconnect', function() {
+    message = 'SocketIO disconnected';
+    console.log(message);
+    logStatus(status_log, message);
+});
+
+socket.on('custom event', function(message) {
+    output = 'Javascript received a custom event: ';
+    console.log(output + message);
+    logStatus(status_log, output + message);
+});
+
+socket.on('message', function(message) {
+    output = 'Javascript received a message: ';
+    console.log(output + message);
+    logStatus(status_log, output + message);
+});
+
+function logStatus(log, message) {
+    log.innerHTML +=
+        "<div><span>" + message + "</span></div>";
+}
+
+function emitString() {
+    event_type = 'javascript_emit'
+    message = "Javascript SocketIO emitted a message.";
+    console.log(message);
+    // emit(event, message)
+    // emit() allows you to send custom event types
+    socket.emit(event_type, message);   // The python and javascript must agree on the message content
+    logStatus(status_log, message);
+}
+
+function sendString() {
+    message = "Javascript SocketIO sent a message.";
+    console.log(message);
+    // send(message)
+    // send() allows you to send messages with the 'message' event
+    socket.send(message);   // There is more to it than this...
+    logStatus(status_log, message);
+}
+
+function emitJson() {
+    event_type = 'javascript_json';
+    message = "Javascript SocketIO emitted JSON.";
+    json = {data: message}
+    console.log(message);
+    console.log(json);
+    socket.emit(event_type, json);
+    logStatus(status_log, message);
+}
+
+// It gets complicated to do this because send() always triggers the 'message' event in python,
+// which would now responsible for parsing both strings and JSON (and anything else we ever send).
+// function sendJson() {
+// }
+
+// The javascript emits the event and the python does nothing.
+function emitUnhandledEvent() {
+    event_type = 'python_does_not_handle';
+    message = 'Javascript SocketIO emitted an event that python does not handle.';
+    console.log(message);
+    socket.emit(event_type, message);
+    logStatus(status_log, message);
+}
+
+function requestEmitCustomEvent() {
+    event_type = 'javascript_request_emit_custom_event';
+    message = "Javascript SocketIO is requesting that Python emit a 'Custom Event'";
+    console.log(message);
+    socket.emit(event_type, message);
+    logStatus(status_log, message);
+}
+
+function requestSend() {
+    event_type = 'javascript_request_send';
+    message = 'Javascript SocketIO is requesting that Python send a message';
+    console.log(message);
+    socket.emit(event_type, message);
+    logStatus(status_log, message);
+}
+
+function requestUnhandleEvent() {
+    event_type = 'javascript_request_unhandled';
+    message = 'Javascript SocketIO is requesting that Python emit an event that Javascript does not handle';
+    console.log(message);
+    socket.emit(event_type, message);
+    logStatus(status_log, message);
+}
--- socket_io_demo/views/default.py
+++ socket_io_demo/views/default.py
@@ -1,6 +1,8 @@
 from flask import render_template, Blueprint, abort
+from flask_socketio import emit, send
 from jinja2 import TemplateNotFound
 from socket_io_demo import app, socketio
+import json
 
 default_view = Blueprint('index', __name__)
 
@@ -14,8 +16,43 @@ def show(page):
 
 @socketio.on('connect')
 def handle_connect():
-    print 'connected'
+    print 'Python SocketIO connected.'
 
-@socketio.on('my event')
-def handle_json(json):
-    print 'received my event: ' + str(json)
+@socketio.on('message')
+def handle_message(message):
+    print 'Python SocketIO received a message: "%s"' % message
+
+@socketio.on('javascript_emit')
+def handle_message(data):   # The python and javascript must agree on the message content
+    print 'Python SocketIO received a javascript_emit: "%s"' % data
+
+@socketio.on('custom event')
+def handle_custom_event(message):
+    print 'SocketIO - custom event: ' + message
+
+@socketio.on('javascript_json')
+def handle_json(json):  # flask-socketio is smart; it receives its message as a dict?
+    print 'Python SocketIO received json: ' + str(json)
+    print 'data: ' + json["data"]
+
+@socketio.on('javascript_request_emit_custom_event')
+def handle_custom_event(message):
+    print 'Python SocketIO received a request to emit a custom event'
+    message = 'Python SocketIO emitted a custom event'
+    print message
+    emit('custom event', message)
+
+@socketio.on('javascript_request_send')
+def handle_send_message(message):
+    print 'Python SocketIO received a request to send a message'
+    message = 'Python SocketIO sent a message'
+    print message
+    send(message)
+
+# The Python emits the event and the javascript does nothing.
+@socketio.on('javascript_request_unhandled')
+def handle_emit_unhandled_event(message):
+    print 'Python SocketIO received a request to emit an unhandled event'
+    message = 'Python SocketIO emitted an event type that Javascript does not know about'
+    print message
+    emit('javascript_does_not_handle', message)
