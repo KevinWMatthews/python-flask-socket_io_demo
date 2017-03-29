7e7807ca9dc4af852d6348ea153756dc8105c516
Kevin Matthews <kmatthews@cyberdata.net>
2017-03-29 10:08:07 -0700

    Add file upload with progress bar

--- socket_io_demo/static/custom_socket_io.js
+++ socket_io_demo/static/custom_socket_io.js
@@ -29,11 +29,6 @@ socket.on('message', function(message) {
     logStatus(status_log, output + message);
 });
 
-function logStatus(log, message) {
-    log.innerHTML +=
-        "<div><span>" + message + "</span></div>";
-}
-
 function emitString() {
     event_type = 'javascript_emit'
     message = "Javascript SocketIO emitted a message.";
@@ -99,4 +94,86 @@ function requestUnhandleEvent() {
     console.log(message);
     socket.emit(event_type, message);
     logStatus(status_log, message);
+}
+
+$(function() {
+    $('#fileupload').fileupload({
+        dataType: 'json',       // Expect the server to return valid json.
+        replaceFileInput: false,
+
+        add: function (e, data) {
+            // Remove upload button if it already exists.
+            $('#upload_button').remove();
+            // Add an 'upload' button.
+            data.context = $('<button/ id="upload_button">').text('Upload')
+                .appendTo("#fileupload_div")
+                .click(function () {
+                    // Replace button with unclickable text once the upload has been started.
+                    console.log('Upload add: user clicked upload button.');
+                    data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                    data.submit();
+                }
+            );
+        },
+
+        submit: function (e, data) {
+            //TODO check if the file exists.
+            //TODO reject files with wrong extension?
+            console.log('Upload submit: ' + data.originalFiles[0].name);
+            $('#fileupload').prop('disabled', true);
+            logStatus(status_log, 'Sending file to server: ' + data.originalFiles[0].name);
+        },
+
+        // The docs say that this generally does not hit because the error callback hits when
+        // the server returns a valid HTTP response? But it seem to be hitting....
+        //TODO if the wrong extension is selected, I don't know where to find the error message.
+        fail: function (e, data) {
+            console.log('Upload fail: ' + data.textStatus + ' ' + data.errorThrown);
+            logError(status_log, 'Upload failed: ' + data.errorThrown);
+        },
+
+        // Not sure about the distinction between the error and fail callbacks,
+        // but fail is giving a more descriptive errorThrown message.
+        /* error: function (e, data) {
+         *  console.log('Upload: ' + data.textStatus + ' ' + data.errorThrown);
+         *  logStatus(status_log, 'Upload error: ' + data.errorThrown);
+        }, */
+
+        progressall: function (e, data) {
+            // This increments the progress bar.
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            updateProgressBar('upload_bar', progress);
+        },
+
+        // This processes the firmware python view's response.
+        done: function (e, data) {
+            console.log('Upload finished!');
+
+            json = data.jqXHR.responseJSON;     // Ok then.
+            console.log(json);
+            if (json.status_message === 'fail') {
+                console.log('Upload failed: ', json.error_message);
+                logError(status_log, 'Upload failed: ' + json.error_message);
+                logError(status_log, 'Upgrade will not complete.');
+                return;
+            }
+            else {
+                logStatus(status_log, 'Upload finished!');
+            }
+        },
+    });
+});
+
+function updateProgressBar(id, progress) {
+    $('#' + id).css('width', progress + '%').attr('aria-valuenow', progress + '%');
+}
+
+function logStatus(log, message) {
+    log.innerHTML +=
+        "<div><span>" + message + "</span></div>";
+}
+
+function logError(log, message) {
+    log.innerHTML +=
+        "<div><span class='text-danger'>" + message + "</span></div>";
 }
--- socket_io_demo/views/threaded.py
+++ socket_io_demo/views/threaded.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, abort, request
+from flask import render_template, Blueprint, abort, request, jsonify
 from werkzeug import secure_filename
 import os
 from flask_socketio import emit, send
@@ -28,22 +28,38 @@ def show_threaded():
         print request.files
 
         if 'uploaded_file' not in request.files:
-            print 'could not find file'
-            return 'could not find file'
+            status_message = 'fail'
+            error_message = 'Requested file not found'
+            print error_message
+            return jsonify(status_message = status_message, error_message = error_message)
 
         print 'getting file'
         file = request.files['uploaded_file']
+        if not file:
+            status_message = 'fail'
+            error_message = 'Uploaded file not found'
+            print error_message
+            return jsonify(status_message = status_message, error_message = error_message)
+
         print 'got file'
         if file.filename == '':
-            print 'File has no filename'
-            return 'File has no filename'
+            status_message = 'fail'
+            error_message = 'Filename not found'
+            print error_message
+            return jsonify(status_message = status_message, error_message = error_message)
 
-        if file: #and allowed_filename(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join('/tmp', filename))
-            return 'OK'
+        # if not allowed_filename(file.filename):
+            # status_message = 'fail'
+            # error_message = 'Filetype not allowed'
+            # print error_message
+            # return jsonify(status_message = status_message, error_message = error_message)
 
-        return 'error'
+        filename = secure_filename(file.filename)
+        filepath = os.path.join('/tmp', filename)
+        file.save(filepath)
+
+        status_message = 'success'
+        return jsonify(status_message = status_message)
 
     # Alternatively, you may use eventlet.spawn(background_thread)
     thread = Thread(target=background_thread)
