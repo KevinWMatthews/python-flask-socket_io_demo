5f7da68931e5c7f775b1d18959f1c05b10259757
Kevin Matthews <kmatthews@cyberdata.net>
2017-03-29 12:00:23 -0700

    Copy in large file example
    https://gitlab.nsd.uib.no/ire/python-webserver-file-submission-poc/blob/master/flask_app.py
    by Ivar Refsdal
    
    These streams are NOT saved anywhere, but you can so stuff with them (say fil.read() in the loop).

--- socket_io_demo/views/threaded.py
+++ socket_io_demo/views/threaded.py
@@ -1,36 +1,43 @@
 from flask import render_template, Blueprint, abort, request
 from werkzeug import secure_filename
-import os
 from jinja2 import TemplateNotFound
 from socket_io_demo import app
+
+import os
+import tempfile
+import time
+
+import flask
+import werkzeug
+import psutil
+
+# from saveserver import current_milli_time, intWithCommas, measure_spent_time
 
 threaded_view = Blueprint('threaded', __name__)
 
 @app.route('/threaded', methods=['GET', 'POST'])
 def show_threaded():
     if request.method == 'POST':
-        print ''
-        print 'Entering post'
-        print request
-        print request.files
+        print 'New POST request'
 
-        if 'uploaded_file' not in request.files:
-            print 'could not find file'
-            return 'could not find file'
+        def custom_stream_factory(total_content_length, filename, content_type, content_length=None):
+            tmpfile = tempfile.NamedTemporaryFile('wb+', prefix='flaskapp')
+            print "start receiving file ... filename => " + str(tmpfile.name)
+            return tmpfile
 
-        print 'getting file'
-        file = request.files['uploaded_file']
-        print 'got file'
-        if file.filename == '':
-            print 'File has no filename'
-            return 'File has no filename'
+        # ms = measure_spent_time()
 
-        if file: #and allowed_filename(file.filename):
-            filename = secure_filename(file.filename)
-            print file.read()
-            return 'OK'
+        stream,form,files = werkzeug.formparser.parse_form_data(flask.request.environ, stream_factory=custom_stream_factory)
+        total_size = 0
 
-        return 'error'
+        for fil in files.values():
+            print " ".join(["saved form name", fil.name, "submitted as", fil.filename, "to temporary file", fil.stream.name])
+            total_size += os.path.getsize(fil.stream.name)
+
+        # mb_per_s = "%.1f" % ((total_size / (1024.0*1024.0)) / ((1.0+ms(raw=True))/1000.0))
+        # print " ".join([str(x) for x in ["handling POST request, spent", ms(), "ms.", mb_per_s, "MB/s.", "Number of files:", len(files.values())]])
+        process = psutil.Process(os.getpid())
+        print "memory usage: %.1f MiB" % (process.memory_info().rss / (1024.0*1024.0))
 
     try:
         return render_template('threaded.html')
