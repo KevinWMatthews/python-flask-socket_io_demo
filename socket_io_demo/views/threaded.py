013e9e5f6416984bf4c27a46de570c5c3eb32ccd
Kevin Matthews <kmatthews@cyberdata.net>
2017-03-29 09:28:06 -0700

    Set up a basic file upload using a form, input, and POST

--- socket_io_demo/views/threaded.py
+++ socket_io_demo/views/threaded.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint, abort
+from flask import render_template, Blueprint, abort, request
+from werkzeug import secure_filename
+import os
 from flask_socketio import emit, send
 from jinja2 import TemplateNotFound
 from socket_io_demo import app, socketio
@@ -17,8 +19,32 @@ def background_thread():
         time.sleep(1)
         send_message('Background thread woke up')
 
-@app.route('/threaded')
+@app.route('/threaded', methods=['GET', 'POST'])
 def show_threaded():
+    if request.method == 'POST':
+        print ''
+        print 'Entering post'
+        print request
+        print request.files
+
+        if 'uploaded_file' not in request.files:
+            print 'could not find file'
+            return 'could not find file'
+
+        print 'getting file'
+        file = request.files['uploaded_file']
+        print 'got file'
+        if file.filename == '':
+            print 'File has no filename'
+            return 'File has no filename'
+
+        if file: #and allowed_filename(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('/tmp', filename))
+            return 'OK'
+
+        return 'error'
+
     # Alternatively, you may use eventlet.spawn(background_thread)
     thread = Thread(target=background_thread)
     thread.daemon = True
