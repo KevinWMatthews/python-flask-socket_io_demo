133378f9cd593728842fc9961de20cef1e143a39
Kevin Matthews <kmatthews@cyberdata.net>
2017-03-29 11:44:16 -0700

    Strip out unneeded stuff

--- socket_io_demo/views/threaded.py
+++ socket_io_demo/views/threaded.py
@@ -1,23 +1,10 @@
 from flask import render_template, Blueprint, abort, request
 from werkzeug import secure_filename
 import os
-from flask_socketio import emit, send
 from jinja2 import TemplateNotFound
-from socket_io_demo import app, socketio
-from threading import Thread
-import time
-import eventlet
-# Event if you are spawning a thread using Thread, you must monkey patch python to emit from a background thread
-eventlet.monkey_patch()
+from socket_io_demo import app
 
 threaded_view = Blueprint('threaded', __name__)
-
-def background_thread():
-    time.sleep(2)
-    send_message('Entering background thread')
-    while True:
-        time.sleep(1)
-        send_message('Background thread woke up')
 
 @app.route('/threaded', methods=['GET', 'POST'])
 def show_threaded():
@@ -45,16 +32,7 @@ def show_threaded():
 
         return 'error'
 
-    # Alternatively, you may use eventlet.spawn(background_thread)
-    thread = Thread(target=background_thread)
-    thread.daemon = True
-    thread.start()
-
     try:
         return render_template('threaded.html')
     except TemplateNotFound:
         abort(404)
-
-def send_message(msg):
-    print msg
-    socketio.emit('message', msg)
